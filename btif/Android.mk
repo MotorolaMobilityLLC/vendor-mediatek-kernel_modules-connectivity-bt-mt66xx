53d45ec86203a1909a79f806a2771684a16a516c
mtk10871 <yu-min.lin@mediatek.com>
2021-04-01 11:35:58 +0800

    [ALPS05532221] LayerDecoupling20: modify kernel makefile
    1. modify kernel makefile
    2. modify pm_qos as runtime flow
    
    MTK-Commit-Id: f2ea2c102fe9725967c99eeb80fb472a4824b728
    
    Change-Id: I7a3f1b49edb0b0a016886a951331e4a0928bc877
    CR-Id: ALPS05532221
    Feature: BT Chipset Capability

--- btif/Android.mk
+++ btif/Android.mk
@@ -1,20 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq (true,$(strip $(TARGET_NO_KERNEL)))
-$(info [BT_Drv][btif] build connac20 btif)
-
 include $(CLEAR_VARS)
-LOCAL_MODULE := bt_drv.ko
+LOCAL_MODULE := bt_drv_$(BT_PLATFORM).ko
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
 LOCAL_INIT_RC := init.bt_drv.rc
 LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%,%,$(shell find $(LOCAL_PATH) -type f -name '*.[cho]')) Makefile
 LOCAL_REQUIRED_MODULES := conninfra.ko
 include $(MTK_KERNEL_MODULE)
-
-BT_OPTS += BT_PLATFORM=$(BT_PLATFORM)
-$(info [BT_Drv][btif] BT_OPTS = $(BT_OPTS))
+BT_OPTS := BT_PLATFORM=$(BT_PLATFORM) LOG_TAG=$(LOG_TAG)
+$(info $(LOG_TAG) BT_OPTS = $(BT_OPTS))
 $(linked_module): OPTS += $(BT_OPTS)
 
-endif
 
