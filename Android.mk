c4a2c15d64576640b42aa5f29677c840aa82fbb9
Simfex Chiu <simfex.chiu@mediatek.com>
2021-07-14 11:53:37 +0800

    [ALPS05792091] BT: Driver porting for MT6983
    Driver porting for MT6983 platform
    
    MTK-Commit-Id: f6d3f7377225e016f0358c09f3e7e0d81897e8a0
    
    Change-Id: If8df110ef54cfdf7e15ea9c2f8918ecb5547ffc0
    Signed-off-by: Simfex Chiu <simfex.chiu@mediatek.com>
    CR-Id: ALPS05792091
    Feature: [Module] BT Driver
    (cherry picked from commit 54c11fc8403f08ca3b69cc222ae1c5c067ca0790)

--- Android.mk
+++ Android.mk
@@ -20,6 +20,8 @@ ifneq (true,$(strip $(TARGET_NO_KERNEL)))
   include $(LOCAL_PATH_B)/btif/Android.mk
   BT_PLATFORM := 6877
   include $(LOCAL_PATH_B)/btif/Android.mk
+  BT_PLATFORM := 6983
+  include $(LOCAL_PATH_B)/btif/Android.mk
 endif
 endif
 
