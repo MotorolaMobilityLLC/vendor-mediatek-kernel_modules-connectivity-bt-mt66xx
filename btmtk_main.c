b403c3d9137c1d6857c985e472dcf89afac73dba
Luke Huang <po-sheng.huang@mediatek.com>
2022-08-24 14:44:32 +0800

    [ALPS07411760] Fixed memory leak issue
    Fixed memory leak issue
    
    MTK-Commit-Id: c6365c938dc5cac8eb44cc481976cfa5862444e7
    
    CR-Id: ALPS07411760
    Feature: BT Driver
    Change-Id: I65596bee28bedc7f10fb068acd059fde7c22d3d1

--- btmtk_main.c
+++ btmtk_main.c
@@ -1199,6 +1199,7 @@ int btmtk_load_code_from_bin(u8 **image, char *bin_name, struct device *dev,
 			*image = NULL;
 			BTMTK_INFO("%s: request_firmware %d times fail, maybe file not exist, err = %d",
 				__func__, 10, err);
+			release_firmware(fw_entry);
 			return -1;
 		}
 		BTMTK_INFO("%s: request_firmware fail, maybe file not exist, err = %d, retry = %d",
