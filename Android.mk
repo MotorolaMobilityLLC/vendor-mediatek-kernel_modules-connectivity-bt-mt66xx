ef4f6a5227b1c35ab58d2594e0f10164fe6ebdf7
Po Hu <po.hu@mediatek.com>
2023-04-15 14:00:29 +0800

    [ALPS06740712] Kleaf: Add rules for MGK kernel module
    [Detail]
    Use Kleaf
    
    MTK-Commit-Id: 3524da5885e13891e6703ee842c4fc0ae9b55633
    
    CR-Id: ALPS06740712
    Feature: build process
    Change-Id: Idb9545ad0910db7cc29ee6b9adad0631305f82a1
    Signed-off-by: Po Hu <po.hu@mediatek.com>

--- Android.mk
+++ Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 LOCAL_PATH_B := $(LOCAL_PATH)
 
-BT_PLATFORM:=$(subst MTK_CONSYS_MT,,$(MTK_BT_CHIP))  
+BT_PLATFORM:=$(subst MTK_CONSYS_MT,,$(MTK_BT_CHIP))
 $(info [BT_Drv] MTK_BT_SUPPORT = $(MTK_BT_SUPPORT))
 $(info [BT_Drv] MTK_BT_CHIP = $(MTK_BT_CHIP))
 
 BT_CONFIG_TRACING=n
 ifeq ($(BT_CONFIG_TRACING),y)
 ifeq ($(CONFIG_TRACING),n)
-	$(error BT_CONFIG_TRACING not support CONFIG_TRACING=n, build error)
+  $(error BT_CONFIG_TRACING not support CONFIG_TRACING=n, build error)
 endif
 endif
 
