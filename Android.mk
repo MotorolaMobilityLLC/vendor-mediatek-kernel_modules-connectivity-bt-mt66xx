4a0dbab1657042de47b0ecc82baac652a50209c0
Simfex Chiu <simfex.chiu@mediatek.com>
2024-12-12 20:18:09 +0800

    [ALPS08662348] BT: MT6899 BT driver porting
    [Description]
    BT driver porting
    
    [Test]
    BT on/off
    
    MTK-Commit-Id: be219b060fc23b9931b966ff89c59b800b1ecbf6
    
    Signed-off-by: Simfex Chiu <simfex.chiu@mediatek.com>
    CR-Id: ALPS08662348
    Feature: BT Driver
    Change-Id: I5b127605b2a65584061f87c85fb2a2723c6c8cef

--- Android.mk
+++ Android.mk
@@ -39,6 +39,8 @@ ifneq (true,$(strip $(TARGET_NO_KERNEL)))
   include $(LOCAL_PATH_B)/btif/Android.mk
   BT_PLATFORM := 6878
   include $(LOCAL_PATH_B)/btif/Android.mk
+  BT_PLATFORM := 6899
+  include $(LOCAL_PATH_B)/btif/Android.mk
 endif
 endif
 
